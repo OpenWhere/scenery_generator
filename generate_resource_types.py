8574c9a5d26ac74bc9a00e42ddc1ae9a1cb1c571
Andrew J. Pierce <apierce@openwhere.com>
2015-02-05 16:51:23 -0500

    Create templates directory for holding templates
    Change the code to reference this template accordingly

--- generate_resource_types.py
+++ generate_resource_types.py
@@ -6,12 +6,15 @@ import os, pprint
 
 DOCS_URL = "http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/"
 CONTENTS_PAGE = "aws-template-resource-type-ref.html"
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def main():
     generator = Generator()
     scraper = Scraper(DOCS_URL)
     classes = scraper.get_documentation_pages(CONTENTS_PAGE)
+    template_file = os.path.join(CURRENT_DIR,
+            'templates', 'resource_type_template.js')
 
     # For each AWS Type, retrieve the list of properties and generate class file
     for key, value in classes.items():
@@ -19,8 +22,7 @@ def main():
         if properties:
             try:
                 property_map = { p['name']: p['type'] for p in properties }
-                generator.create_class_file('./resource_type_template.js',
-                                            key, property_map)
+                generator.create_class_file(template_file, key, property_map)
             except:
                 print("Failed to generate file for %s: %s" % (key, properties))
 
