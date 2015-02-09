ebeb4b01fc481b8e1bac28f6802b846dcc4d8261
Andrew J. Pierce <apierce@openwhere.com>
2015-02-08 19:26:56 -0500

    Properties are now correctly referenced in property maps

--- scraper.py
+++ scraper.py
@@ -14,31 +14,33 @@ class Scraper(object):
         on the page map its text to it's href
         Returns a dictionary in the format { "link text": "docs_page.html", ...}
         """
-        docs_page = urlopen("%s/%s" % (self.docs_url, toc_page))
+        docs_page = urlopen('%s/%s' % (self.docs_url, toc_page))
         soup = BeautifulSoup(docs_page)
-        links = soup.find_all('a')
+        links = soup.find('div', { 'class' : 'highlights' }).findAll('a')
 
         # Parse the list of all classes to get names and URLS
         pages = {}
         for link in links:
             if not link.getText():
                 continue
-            cleanKey = link.getText().replace('\n', '').replace(' ', '')
-            pages[cleanKey] = link['href']
+            title = link.getText().replace('\n', '').replace(' ', '')
+            href = link['href'].replace('.html', '').strip()
+            clean_key = "%s (%s)" % (title, href)
+            pages[clean_key] = link['href']
 
         return pages
 
 
     def get_properties(self, page_url):
-        print("Fetching: %s" % page_url)
-        class_page = urlopen("%s/%s" % (self.docs_url, page_url))
+        print('Fetching: %s' % page_url)
+        class_page = urlopen('%s/%s' % (self.docs_url, page_url))
         if class_page:
             property_types = []
             soup = BeautifulSoup(class_page)
             try:
                 properties_list = soup.dl.contents
             except:
-                print("No docs for page %s" % page_url)
+                print('No docs for page %s' % page_url)
                 return None
 
             # Break the docs up into sections by property
@@ -55,7 +57,6 @@ class Scraper(object):
             # Create a dictionary for each property type
             for s in sections:
                 property_dict = {
-                    'href': None,
                     'list': False,
                     'name': s[0].getText(),
                     'type': 'String'
@@ -71,13 +72,13 @@ class Scraper(object):
                                 .replace('Type:', '')\
                                 .replace('.', '')\
                                 .replace('\n', '').strip()
-                        if 'list of' in clean_text.lower():
+                        if 'list' in clean_text.lower():
                             property_dict['list'] = True
-                            clean_text = clean_text.lower()\
-                                    .replace('a list of', '')\
-                                    .replace('list of', '').strip().title()
+                            lower_text = clean_text.lower()
+                            clean_text = re.sub('a*\s*list\s*(of)*', '', clean_text)\
+                                    .strip().title()
                         if p.a:
-                            property_dict['href'] = p.a.get('href')
+                            clean_text = p.a.get('href').replace('.html','').strip()
                         property_dict['type'] = self._get_type(clean_text)
 
                 property_types.append(property_dict)
